e3ec7a2cbc3c26517516ea4b8778d5aec5618dc8
Joshua Hebert <jhebertcocktails@gmail.com>
2023-07-05 21:59:49 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
+// Import necessary packages
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
+const path = require('path');
 const htmlRoutes = require('./routes/htmlRoutes');
-
-const app = express();
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 
+const app = express();
+
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
+
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+
+app.use(htmlRoutes);
+
+app.use(apiRoutes);
 
 app.listen(PORT, () => {
-    console.log(`App listening on PORT ${PORT}!`);
+  console.log(`App listening on PORT ${PORT}!`);
 });
+
